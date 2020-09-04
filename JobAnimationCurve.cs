ee64bffb458e06f7e5a7a75466997c0139737e29
apkd <apkd@users.noreply.github.com>
2020-09-04 18:37:17 +0200

    Cleanup, refactor and optimize

--- JobAnimationCurve.cs
+++ JobAnimationCurve.cs
@@ -1,219 +1,124 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Unity.Collections;
-using Unity.Mathematics;
 using Unity.Collections.LowLevel.Unsafe;
-using UnityEngine;
 using Unity.Entities;
+using Unity.Jobs.LowLevel.Unsafe;
+using Unity.Mathematics;
+using UnityEngine;
 
-namespace E7.DataStructure
+/// <summary>
+/// Struct representing an <see cref="AnimationCurve"/> that can be used with the Burst Compiler..
+/// </summary>
+public struct NativeAnimationCurve : IDisposable
 {
-    /// <summary>
-    /// Immutable curve completely disconnected from the original <see cref="AnimationCurve"> once instantiated.
-    /// Post/pre wrap mode not supported (yet).
-    /// Curve weight not supported, until I figured out what is the underlying function.
-    /// </summary>
-    public struct JobAnimationCurve : IDisposable
+    struct AnimationData
     {
-        private struct AnimationData
-        {
-            public BlobArray<Keyframe> keyframes;
-            //This is a hack to help stupid linear interval search, ideally we should find a better way to land on the interval without linear search in the first place.
-            public BlobArray<float> soaTimes;
-            //More hack to help the stupid linear search, cached index is for my observation that we usually evaluate on the same interval or the next one. 
-            //But ideally we have to find better way to land on the correct interval fast without this. (interval tree?)
-            public BlobArray<int> cachedIndex;
-        }
+        public BlobArray<Keyframe> keyframes;
 
-        private BlobAssetReference<AnimationData> bar;
+        // This is a hack to help stupid linear interval search, ideally we should find a better way to land on the interval without linear search in the first place.
+        public BlobArray<float> soaTimes;
 
-        // [ReadOnly] [NativeDisableParallelForRestriction] NativeArray<Keyframe> keyframes;
-        // [ReadOnly] [NativeDisableParallelForRestriction] NativeArray<float> soaTimes;
-
-        //Not supported yet..
-        private WrapMode postWrapMode;
-        private WrapMode preWrapMode;
-        private InterpolationMode interpolationMode;
-
-        /// <summary>
-        /// Used for caching optimization. Each thread would get its own cache area which is selected based on this integer.
-        /// </summary>
-        [NativeSetThreadIndex] private int threadIndex;
-
-        public int length => bar.Value.keyframes.Length;
-
-        /// <summary>
-        /// These are currently useless, just a plan draft..
-        /// </summary>
-        public enum InterpolationMode
-        {
-            /// <summary>
-            /// This is the same thing as Unity's <see cref="AnimationCurve.Evaluate(float)">. The most accurate but also the most time consuming.
-            /// </summary>
-            CubicHermiteSpline,
-
-            /// <summary>
-            /// Not exactly like <see cref="AnimationCurve.Evaluate(float)">, each hermite basis function are approximated.
-            /// </summary>
-            ApproximatedCubicHermiteSpline,
-
-            /// <summary>
-            /// A horrible optimization that just interpolates as if a straight line is in between every nodes. It is certainly fast though!
-            /// 
-            /// You are recommended to use <see cref="IncreaseResolution(float)"> first before <see cref="Evaluate(float)"> while in this mode.
-            /// So that points that are far apart get more nodes to connect, making linear interpolation more reasonable at the cost of memory.
-            /// (If you know you have a highly curved path on far-apart pair of nodes, that will get the most error when linearly interpolated.)
-            /// </summary>
-            Linear,
-        }
-
-        /// <summary>
-        /// Replicate all keyframes of <paramref name="animationCurve"> into the struct, 
-        /// making this independent from the original reference type curve.
-        /// </summary>
-        public JobAnimationCurve(AnimationCurve animationCurve, Allocator allocator)
-        {
-            List<Keyframe> sortedKeyframes = new List<Keyframe>(animationCurve.keys);
-            if (sortedKeyframes.Any(x => x.weightedMode != WeightedMode.None))
-            {
-                throw new NotSupportedException($"Found a keyframe in the curve that has a weighted node. This is not supported until I figured out where to put the weight.");
-            }
-            sortedKeyframes.Sort(KeyframeTimeSort);
-
-            //Debug.Log(string.Join("\n", sortedKeyframes.Select(x => $"{x.time} {x.value} | {x.inTangent} {x.outTangent} | {x.inWeight} {x.outWeight} {x.weightedMode}")));
-
-            var sortedTimes = sortedKeyframes.Select(x => x.time).ToArray();
-
-            using (var ba = new BlobAllocator(-1))
-            {
-                ref var root = ref ba.ConstructRoot<AnimationData>();
-                int processorCount = SystemInfo.processorCount + 1;
-                ba.Allocate(sortedKeyframes.Count, ref root.keyframes);
-                ba.Allocate(sortedKeyframes.Count, ref root.soaTimes);
-                ba.Allocate(processorCount, ref root.cachedIndex);
-                for (int i = 0; i < sortedKeyframes.Count; i++)
-                {
-                    root.keyframes[i] = sortedKeyframes[i];
-                    root.soaTimes[i] = sortedTimes[i];
-                }
-                for (int i = 0; i < processorCount; i++)
-                {
-                    root.cachedIndex[i] = 0;
-                }
-
-                bar = ba.CreateBlobAssetReference<AnimationData>(allocator);
-            }
-
-            postWrapMode = animationCurve.postWrapMode;
-            preWrapMode = animationCurve.preWrapMode;
-            interpolationMode = InterpolationMode.CubicHermiteSpline;
-            threadIndex = 0;
-
-            int KeyframeTimeSort(Keyframe a, Keyframe b) => a.time.CompareTo(b.time);
-        }
-
-        /// <summary>
-        /// Add more points to the curve while preserving its shape.
-        /// </summary>
-        /// <param name="minimalInterval">Smallest time interval allowed in the curve. This method will add points in between if it found an interval larger than this.</param>
-        public float IncreaseResolution(float minimalInterval)
-        {
-            throw new NotImplementedException();
-        }
-
-        public float Evaluate(float time)
-        {
-            //TODO : Use interval tree to find neighbouring keyframes of a given time.
-            //for (int i = 0; i < bar.Value.soaTimes.Length; i++)
-            for (int i = bar.Value.cachedIndex[threadIndex], count = 0;
-                count < bar.Value.soaTimes.Length;
-                count++, i = (i + 1) % bar.Value.soaTimes.Length)
-            {
-                //Debug.Log($"Finding at {i} count {count}");
-                if (time >= bar.Value.soaTimes[i] && time < bar.Value.soaTimes[i + 1])
-                {
-                    //cachedIndex[threadIndex] = i;
-                    //Debug.Log($"{time} Using keyframe {i} ({soaTimes[i]}) and {i + 1} ({soaTimes[i + 1]})");
-                    // System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
-                    // sw.Start();
-                    var eval = EvaluateInternal2(
-                        time,
-                        bar.Value.keyframes[i].time,
-                        bar.Value.keyframes[i].value,
-                        bar.Value.keyframes[i].outTangent,
-                        bar.Value.keyframes[i + 1].time,
-                        bar.Value.keyframes[i + 1].value,
-                        bar.Value.keyframes[i + 1].inTangent
-                    );
-                    // sw.Stop();
-                    // Debug.Log($"TICKS {sw.ElapsedTicks}");
-                    return eval;
-                }
-                
-            }
-
-            throw new NotSupportedException($"Interval for time {time} not found! Until WrapMode is supported, you must use time that falls inside the curve's data.");
-        }
-
-        /// <summary>
-        /// This is cubic hermite spline
-        /// https://en.wikipedia.org/wiki/Cubic_Hermite_spline
-        /// 
-        /// i = interval
-        /// v = value
-        /// t = tangent
-        /// 
-        /// The function is written unexpanded for easier understanding, 
-        /// since I saw Burst is not doing any better with expanded equation (at the current version).
-        /// </summary>
-        private float EvaluateInternal(float iInterp, 
-        float iLeft, float vLeft, float tLeft, 
-        float iRight, float vRight, float tRight)
-        {
-            float t = math.unlerp(iLeft, iRight, iInterp);
-
-            //TODO: This could be precalculated from when we are creating this struct for each interval? Micro optimization?
-            float scale = iRight - iLeft;
-
-            //TODO: Maybe we could create an approximation of each hermite basis that blends into an answer?
-            float h00(float x) => (2 * x * x * x) - (3 * x * x) + 1;
-            float h10(float x) => (x * x * x) - (2 * x * x) + x;
-            float h01(float x) => -(2 * x * x * x) + (3 * x * x);
-            float h11(float x) => (x * x * x) - (x * x);
-
-            //TODO: Scaled tangents could also be precalculated. Micro optimization?
-            return (h00(t) * vLeft) + (h10(t) * scale * tLeft) + (h01(t) * vRight) + (h11(t) * scale * tRight);
-        }
-
-        /// <summary>
-        /// Matrix based version in hope that Burst could do better.
-        /// </summary>
-        private float EvaluateInternal2(float iInterp, 
-        float iLeft, float vLeft, float tLeft, 
-        float iRight, float vRight, float tRight)
-        {
-            float t = math.unlerp(iLeft, iRight, iInterp);
-            float scale = iRight - iLeft;
-
-            float4 parameters = new float4(t * t * t, t * t, t, 1);
-            float4x4 hermiteBasis = new float4x4(
-                2, -2,  1,  1,
-                -3, 3, -2, -1,
-                0,  0,  1,  0,
-                1,  0,  0,  0
-            );
-
-            //TODO : Tangent could be prescaled with also precalculated interval size.
-            float4 control = new float4(vLeft, vRight, tLeft, tRight) * new float4(1, 1, scale, scale);
-            float4 basisWithParams = math.mul(parameters, hermiteBasis);
-            float4 hermiteBlend = control * basisWithParams;
-            return math.csum(hermiteBlend);
-        }
-
-        public void Dispose()
-        {
-            bar.Release();
-        }
+        // More hack to help the stupid linear search, cached index is for my observation that we usually evaluate on the same interval or the next one. 
+        // But ideally we have to find better way to land on the correct interval fast without this. (interval tree?)
+        public BlobArray<int> cachedIndex;
     }
+
+    readonly struct KeyframeTimeComparer : IComparer<Keyframe>
+    {
+        public int Compare(Keyframe x, Keyframe y)
+            => x.time.CompareTo(y.time);
+    }
+
+    BlobAssetReference<AnimationData> animationDataBlob;
+
+    /// <summary> Used for caching optimization. Each thread would get its own cache area which is selected based on this integer. </summary>
+    [NativeSetThreadIndex]
+    readonly int threadIndex;
+
+    public int Length
+        => animationDataBlob.Value.keyframes.Length;
+
+    /// <summary> Create a new NativeAnimationCurve based on the keyframes from the <paramref name="animationCurve">. </summary>
+    public NativeAnimationCurve(AnimationCurve animationCurve, Allocator allocator)
+    {
+        using (var sortedKeyframes = new NativeArray<Keyframe>(animationCurve.keys, Allocator.Temp))
+        {
+            for (int i = 0, n = sortedKeyframes.Length; i < n; i++)
+                if (sortedKeyframes[i].weightedMode != WeightedMode.None)
+                    throw new NotSupportedException("Found a keyframe in the curve that has a weighted node. This is not supported.");
+
+            sortedKeyframes.Sort(default(KeyframeTimeComparer));
+
+            using (var builder = new BlobBuilder(Allocator.Temp))
+            {
+                ref var root = ref builder.ConstructRoot<AnimationData>();
+
+                var keyframes = builder.Allocate(ref root.keyframes, sortedKeyframes.Length);
+                var soaTimes = builder.Allocate(ref root.soaTimes, sortedKeyframes.Length);
+                var cachedIndex = builder.Allocate(ref root.cachedIndex, JobsUtility.MaxJobThreadCount);
+
+                for (int i = 0; i < sortedKeyframes.Length; i++)
+                {
+                    keyframes[i] = sortedKeyframes[i];
+                    soaTimes[i] = sortedKeyframes[i].time;
+                }
+
+                for (int i = 0; i < JobsUtility.MaxJobThreadCount; i++)
+                    cachedIndex[i] = 0;
+
+                animationDataBlob = builder.CreateBlobAssetReference<AnimationData>(allocator);
+            }
+        }
+
+        threadIndex = 0;
+    }
+
+    public float Evaluate(float time)
+    {
+        ref var animationData = ref animationDataBlob.Value;
+
+        int n = animationData.soaTimes.Length;
+        for (int i = animationData.cachedIndex[threadIndex], count = 0; count < n; count++, i = (i + 1) % n)
+        {
+            if (time < animationData.soaTimes[i] || time >= animationData.soaTimes[i + 1])
+                continue;
+
+            var left = animationData.keyframes[i];
+            var right = animationData.keyframes[i + 1];
+            animationData.cachedIndex[threadIndex] = i;
+
+            return EvaluateInternal(time, left.time, left.value, left.outTangent, right.time, right.value, right.inTangent);
+        }
+
+        return float.NaN;
+    }
+
+    static float EvaluateInternal(float iInterp, float iLeft, float vLeft, float tLeft, float iRight, float vRight, float tRight)
+    {
+        var t = math.unlerp(iLeft, iRight, iInterp);
+        var scale = iRight - iLeft;
+
+        var parameters = new float4(
+            t * t * t,
+            t * t,
+            t,
+            1
+        );
+
+        var hermiteBasis = new float4x4(
+            +2, -2, +1, +1,
+            -3, +3, -2, -1,
+            +0, +0, +1, +0,
+            +1, +0, +0, +0
+        );
+
+        var control = new float4(vLeft, vRight, tLeft, tRight) * new float4(1, 1, scale, scale);
+        var basisWithParams = math.mul(parameters, hermiteBasis);
+        var hermiteBlend = control * basisWithParams;
+        return math.csum(hermiteBlend);
+    }
+
+    public void Dispose()
+        => animationDataBlob.Dispose();
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# JobAnimationCurve
+# NativeAnimationCurve
 
-Currently the performance sucks but it did give equal answer (without weight) to `AnimationCurve` and works in a job.
+Cleaned up version of [5argon/JobAnimationCurve](https://github.com/5argon/JobAnimationCurve), with a couple of optimizations and same overall features and drawbacks (no weighted tangents and no wrapping).
+
+This is currently faster than an AnimationCurve when used in conjunction with the Burst Compiler, but significantly slower in Mono.
